6a80985da50131d5fb2b4f47415fdcf5ef8e0841
sunrise <575563079@qq.com>
2023-02-12 17:12:08 +0800

    init the repo

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	app := gin.Default()
+	gin.SetMode(gin.DebugMode)
+
+	//模板处理
+	app.LoadHTMLGlob("website/templates/**/*")
+	app.Static("/static", "website/static")
+	app.GET("/", func(c *gin.Context) {
+		// 子目录的模版文件，需要加上目录名，例如：posts/index.tmpl
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Posts",
+		})
+	})
+	app.GET("/404", func(c *gin.Context) {
+		// 子目录的模版文件，需要加上目录名，例如：users/index.tmpl
+		c.HTML(http.StatusOK, "error/404.html", gin.H{
+			"title": "Users",
+		})
+	})
+
+	app.Run(":8080")
+}
