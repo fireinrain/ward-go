6f94e8fe8c89247e84d0e4389a76879d070adc26
sunrise <575563079@qq.com>
2023-03-06 16:16:12 +0800

    fix cant display panel

--- main.go
+++ main.go
@@ -6,24 +6,31 @@ import (
 )
 
 func main() {
+	// 初始化一个http服务对象
 	app := gin.Default()
-	gin.SetMode(gin.DebugMode)
 
-	//模板处理
-	app.LoadHTMLGlob("website/templates/*/*")
+	// 首先加载templates目录下面的所有模版文件，模版文件扩展名随意
+	app.LoadHTMLGlob("website/templates/*")
 	app.Static("/static", "website/static")
-	app.GET("/", func(c *gin.Context) {
-		// 子目录的模版文件，需要加上目录名，例如：posts/index.tmpl
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Posts",
-		})
-	})
-	app.GET("/404", func(c *gin.Context) {
-		// 子目录的模版文件，需要加上目录名，例如：users/index.tmpl
-		c.HTML(http.StatusOK, "404.html", gin.H{
-			"title": "Users",
+
+	// 设置一个get请求的路由，url为/ping, 处理函数（或者叫控制器函数）是一个闭包函数。
+	app.GET("/ping", func(c *gin.Context) {
+		// 通过请求上下文对象Context, 直接往客户端返回一个json
+		c.JSON(200, gin.H{
+			"message": "pong",
 		})
 	})
 
-	_ = app.Run(":8080")
+	// 绑定一个url路由 /index
+	app.GET("/", func(c *gin.Context) {
+		// 通过HTML函数返回html代码
+		// 第二个参数是模版文件名字
+		// 第三个参数是map类型，代表模版参数
+		// gin.H 是map[string]interface{}类型的别名
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+
+	app.Run("0.0.0.0:8888") // 监听并在 0.0.0.0:8080 上启动服务
 }
