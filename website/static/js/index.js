8f604eab94004d9013f61add73d1efe515b0cbf3
sunrise <575563079@qq.com>
2023-03-07 17:54:36 +0800

    优化

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -3,136 +3,125 @@
 /**
  * Initializes uptime, labels and chart values
  */
-function indexInitialization()
-{
-    logoPage = document.getElementById("logo-page");
-    contactsPage = document.getElementById("contacts-page");
+function indexInitialization() {
+  logoPage = document.getElementById("logo-page");
+  contactsPage = document.getElementById("contacts-page");
 
-    showCards();
+  showCards();
 
-    currentClockSpeed = document.getElementById("currentClockSpeed");
-    currentProcCount = document.getElementById("currentProcCount");
-    currentTotalStorage = document.getElementById("currentTotalStorage");
-    currentDiskCount = document.getElementById("currentDiskCount");
+  currentClockSpeed = document.getElementById("currentClockSpeed");
+  currentProcCount = document.getElementById("currentProcCount");
+  currentTotalStorage = document.getElementById("currentTotalStorage");
+  currentDiskCount = document.getElementById("currentDiskCount");
 
-    currentPage = 1;
-    firstControl = document.getElementById("first-control");
-    secondControl = document.getElementById("second-control");
+  currentPage = 1;
+  firstControl = document.getElementById("first-control");
+  secondControl = document.getElementById("second-control");
 
-    cloudLeft = document.getElementById("cloud-left");
-    cloudRight = document.getElementById("cloud-right");
+  cloudLeft = document.getElementById("cloud-left");
+  cloudRight = document.getElementById("cloud-right");
 
-    days = document.getElementById("uptime-days");
-    hours = document.getElementById("uptime-hours");
-    minutes = document.getElementById("uptime-minutes");
-    seconds = document.getElementById("uptime-seconds");
+  days = document.getElementById("uptime-days");
+  hours = document.getElementById("uptime-hours");
+  minutes = document.getElementById("uptime-minutes");
+  seconds = document.getElementById("uptime-seconds");
 
-    usageXHR = new XMLHttpRequest();
-    infoXHR = new XMLHttpRequest();
+  usageXHR = new XMLHttpRequest();
+  infoXHR = new XMLHttpRequest();
 
-    setInterval(function (){
-        sendUsageRequest();
-    },2100)
+  setInterval(function () {
+    sendUsageRequest();
+  }, 2100)
 
-    setInterval(function()
-    {
-        sendInfoRequest();
-    }, 2800);
+  setInterval(function () {
+    sendInfoRequest();
+  }, 2800);
 
-    firstControl.addEventListener("click", function(event) {changePage(event.target || event.srcElement)});
-    secondControl.addEventListener("click", function(event) {changePage(event.target || event.srcElement)});
+  firstControl.addEventListener("click", function (event) {
+    changePage(event.target || event.srcElement)
+  });
+  secondControl.addEventListener("click", function (event) {
+    changePage(event.target || event.srcElement)
+  });
 }
 
 /**
  * Changes cards opacity with random sequence
  */
-function showCards()
-{
-    contactsPage.style.visibility = "hidden";
+function showCards() {
+  contactsPage.style.visibility = "hidden";
 
-    let cards = document.getElementsByClassName("card");
-    let versionLabel = document.getElementById("project-version");
+  let cards = document.getElementsByClassName("card");
+  let versionLabel = document.getElementById("project-version");
 
-    let randomSequenceArray = getRandomSequenceArray();
+  let randomSequenceArray = getRandomSequenceArray();
 
-    for (let i = 0; i < cards.length; i++)
-    {
-        setTimeout(function()
-        {
-            cards[randomSequenceArray[i]].style.opacity = "1";
+  for (let i = 0; i < cards.length; i++) {
+    setTimeout(function () {
+      cards[randomSequenceArray[i]].style.opacity = "1";
 
-            if (randomSequenceArray[i] == 4)
-            {
-                versionLabel.style.opacity = "1";
-            }
-        }, 70 * i);
-    }
+      if (randomSequenceArray[i] == 4) {
+        versionLabel.style.opacity = "1";
+      }
+    }, 70 * i);
+  }
 }
 
 /**
  * Generates random sequence
  */
-function getRandomSequenceArray()
-{
-    let buffer = [];
+function getRandomSequenceArray() {
+  let buffer = [];
 
-    while (buffer.length < 5)
-    {
-        let randomNumber = Math.floor(Math.random() * 5);
+  while (buffer.length < 5) {
+    let randomNumber = Math.floor(Math.random() * 5);
 
-        if ((buffer.indexOf(randomNumber) === -1))
-        {
-            buffer.push(randomNumber);
-        }
+    if ((buffer.indexOf(randomNumber) === -1)) {
+      buffer.push(randomNumber);
     }
+  }
 
-    return buffer;
+  return buffer;
 }
 
 /**
  * Sending ajax request to receive usage info
  */
-function sendUsageRequest()
-{
-    usageXHR.onreadystatechange = function()
-    {
-        if ((this.readyState == 4) && (this.status == 200))
-        {
-            let response = JSON.parse(this.response);
-            labelsTick(response);
-            chartTick(response);
-        }
+function sendUsageRequest() {
+  usageXHR.onreadystatechange = function () {
+    if ((this.readyState == 4) && (this.status == 200)) {
+      let response = JSON.parse(this.response);
+      labelsTick(response);
+      chartTick(response);
     }
+  }
 
-    usageXHR.open("GET", "/api/usage");
-    usageXHR.send();
+  usageXHR.open("GET", "/api/usage");
+  usageXHR.send();
 }
 
 /**
  * Sending ajax request to receive info about server
  */
-function sendInfoRequest()
-{
-    infoXHR.onreadystatechange = function()
-    {
-        if ((this.readyState == 4) && (this.status == 200))
-        {
-            let response = JSON.parse(this.response);
+function sendInfoRequest() {
+  infoXHR.onreadystatechange = function () {
+    if ((this.readyState == 4) && (this.status == 200)) {
+      let response = JSON.parse(this.response);
 
-            currentClockSpeed.innerHTML = response.processor.clockSpeed;
-            currentProcCount.innerHTML = response.machine.procCount;
-            currentTotalStorage.innerHTML = response.storage.total;
-            currentDiskCount.innerHTML = response.storage.diskCount;
+      currentClockSpeed.innerHTML = response.processor.clockSpeed;
+      currentProcCount.innerHTML = response.machine.procCount;
+      currentTotalStorage.innerHTML = response.storage.total;
+      currentDiskCount.innerHTML = response.storage.diskCount;
 
-            days.innerHTML = response.uptime.days;
-            hours.innerHTML = response.uptime.hours;
-            minutes.innerHTML = response.uptime.minutes;
-            seconds.innerHTML = response.uptime.seconds;
-        }
+      days.innerHTML = response.uptime.days;
+      hours.innerHTML = response.uptime.hours;
+      minutes.innerHTML = response.uptime.minutes;
+      seconds.innerHTML = response.uptime.seconds;
     }
+  }
 
-    infoXHR.open("GET", "/api/info");
-    infoXHR.send();
+  infoXHR.open("GET", "/api/info");
+  infoXHR.send();
 }
 
 /**
@@ -140,21 +129,17 @@ function sendInfoRequest()
  *
  * @param {*} control element
  */
-function changePage(element)
-{
-    if ((String(element.id) == "first-control") && (currentPage > 1))
-    {
-        currentPage -= 1;
-        setCloudAnimation(currentPage);
-    }
-    else if ((String(element.id) == "second-control") && (currentPage < 2))
-    {
-        currentPage += 1;
-        setCloudAnimation(currentPage);
-    }
+function changePage(element) {
+  if ((String(element.id) == "first-control") && (currentPage > 1)) {
+    currentPage -= 1;
+    setCloudAnimation(currentPage);
+  } else if ((String(element.id) == "second-control") && (currentPage < 2)) {
+    currentPage += 1;
+    setCloudAnimation(currentPage);
+  }
 
-    setPageVisibility(currentPage);
-    setControlOpacity(currentPage);
+  setPageVisibility(currentPage);
+  setControlOpacity(currentPage);
 }
 
 /**
@@ -162,23 +147,19 @@ function changePage(element)
  *
  * @param {*} new page
  */
-function setPageVisibility(newPage)
-{
-    switch (newPage)
-    {
-        case 1:
-        {
-            logoPage.style.visibility = "";
-            contactsPage.style.visibility = "hidden";
-            break;
-        }
-        case 2:
-        {
-            logoPage.style.visibility = "hidden";
-            contactsPage.style.visibility = "";
-            break;
-        }
+function setPageVisibility(newPage) {
+  switch (newPage) {
+    case 1: {
+      logoPage.style.visibility = "";
+      contactsPage.style.visibility = "hidden";
+      break;
     }
+    case 2: {
+      logoPage.style.visibility = "hidden";
+      contactsPage.style.visibility = "";
+      break;
+    }
+  }
 }
 
 /**
@@ -186,23 +167,19 @@ function setPageVisibility(newPage)
  *
  * @param {*} new page
  */
-function setCloudAnimation(newSquareScale)
-{
-    switch (newSquareScale)
-    {
-        case 1:
-        {
-            cloudLeft.style.animation = "fade-in-cloud-left 0.3s forwards";
-            cloudRight.style.animation = "fade-in-cloud-right 0.3s forwards";
-            break;
-        }
-        case 2:
-        {
-            cloudLeft.style.animation = "fade-out-cloud-left 0.3s forwards";
-            cloudRight.style.animation = "fade-out-cloud-right 0.3s forwards";
-            break;
-        }
+function setCloudAnimation(newSquareScale) {
+  switch (newSquareScale) {
+    case 1: {
+      cloudLeft.style.animation = "fade-in-cloud-left 0.3s forwards";
+      cloudRight.style.animation = "fade-in-cloud-right 0.3s forwards";
+      break;
     }
+    case 2: {
+      cloudLeft.style.animation = "fade-out-cloud-left 0.3s forwards";
+      cloudRight.style.animation = "fade-out-cloud-right 0.3s forwards";
+      break;
+    }
+  }
 }
 
 /**
@@ -210,21 +187,17 @@ function setCloudAnimation(newSquareScale)
  *
  * @param {*} new page
  */
-function setControlOpacity(newSquareScale)
-{
-    switch (newSquareScale)
-    {
-        case 1:
-        {
-            firstControl.style.opacity = "0.5";
-            secondControl.style.opacity = "1";
-            break;
-        }
-        case 2:
-        {
-            firstControl.style.opacity = "1";
-            secondControl.style.opacity = "0.5";
-            break;
-        }
+function setControlOpacity(newSquareScale) {
+  switch (newSquareScale) {
+    case 1: {
+      firstControl.style.opacity = "0.5";
+      secondControl.style.opacity = "1";
+      break;
     }
+    case 2: {
+      firstControl.style.opacity = "1";
+      secondControl.style.opacity = "0.5";
+      break;
+    }
+  }
 }
